5a95f2ba69ccf2b3063dc9915fcf736dfe0e47ea
564754 <tekindy@hotmail.com>
2026-03-10 15:38:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def robust_yf_download(
                             downloaded += 1
                     except: # <--- Bu satır tam olarak 'try' ile aynı hizada olmalı
                         failed_tickers.append(t)
-         break  # Başarılıysa retry'dan çık
+            break  # Başarılıysa retry'dan çık
             except Exception as e: # <--- Bu satır 'try: raw = yf.download' ile aynı hizada olmalı
                 if attempt == max_retries  - 1:
                     failed_tickers.extend(chunk)
